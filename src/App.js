967f647cf9fc88c16c6e5a702b8418c2fcca6a7d
rahul mathur <rmathur.028@gmail.com>
2023-05-10 23:31:46 +0530

    Todo App Using React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
 import Details from "./components/Details";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -15,6 +16,12 @@ function App() {
     setDataji([...dataji, { name, email }]);
     setName("");
     setEmail("");
+  };
+
+  const removeData = (index) => {
+    let arr = dataji;
+    arr.splice(index, 1);
+    setDataji([...arr]);
   };
 
   return (
@@ -49,12 +56,20 @@ function App() {
         </div>
         {dataji.map((element, index) => {
           return (
-            <Details
-              key={index}
-              name={element.name}
-              email={element.email}
-              index={index}
-            />
+            <div key={index} className="entry">
+              <h5>{element.name}</h5>
+              <h5>{element.email}</h5>
+              <Stack>
+                <Button
+                  onClick={() => removeData(index)}
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                  color="error"
+                >
+                  Delete
+                </Button>
+              </Stack>
+            </div>
           );
         })}
       </div>
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
 const Details = ({ name, email, index }) => {
   return (
     <div className="entry">
       <h5>{name}</h5>
       <h5>{email}</h5>
-      <h5>Remove</h5>
+      <Stack>
+        <Button variant="outlined" startIcon={<DeleteIcon />} color="error">
+          Delete
+        </Button>
+      </Stack>
     </div>
   );
 };
